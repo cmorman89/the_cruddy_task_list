5f871f3df4e26b7c2645f26939d054d47ba0910c
Charles Morman <github@cmorman.com>
2024-11-20 22:17:07 -0700

    tests: `Task` construction and `TaskID` uniqueness tests implemented Ref #1, #7

---
+++ tests/test_task.py
@@ -0,0 +1,57 @@
+"""
+Tests for the `Task` Module
+"""
+
+from datetime import datetime
+
+import pytest
+
+from app.task import Task
+
+
+@pytest.mark.parametrize(
+    "title, description, due_date, status, exp_due_date, exp_status",
+    [
+        ("Task Title", None, None, None, "", "Pending"),
+        (
+            "Task Title",
+            "The longer description of the task.",
+            datetime(2024, 12, 31),
+            "Completed",
+            datetime(2024, 12, 31),
+            "Completed",
+        ),
+    ],
+)
+def test_task_construction(
+    title, description, due_date, status, exp_due_date, exp_status
+):
+    """Test basic `Task` construction, including minimal and full arguments provided"""
+    task = Task(title=title, description=description, due_date=due_date, status=status)
+    assert task.task_id >= 0
+    assert task.title == title
+    assert task.description == description if description else task.description is None
+    if due_date:
+        assert task.due_date == exp_due_date
+    else:
+        assert isinstance(task.due_date, datetime)
+    assert task.status == exp_status
+
+
+def test_task_id_uniqueness():
+    """Generate several `Task` objects and check that each ID is unique."""
+    task_1 = Task("task 1")
+    task_2 = Task("task 2")
+    task_3 = Task("task 3")
+
+    assert task_1.task_id != task_2.task_id != task_3.task_id
+
+
+def test_task_id_immutable():
+    """Test that `task_id` cannot be set from public interface after init"""
+    task = Task("task")
+    try:
+        task.task_id = 1
+        assert False
+    except:
+        assert True
---
+++ tests/test_task_id.py
@@ -0,0 +1,7 @@
+from app.task_id import TaskID
+
+def test_id_generation():
+    """Generates a list of IDs and checks if they are unqiue by converting to set and comparing
+    the length before and after"""
+    id_list = [TaskID.generate() for id in range(100)]
+    assert len(id_list) == len(set(id_list))
