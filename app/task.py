8cf121fd35c444b749a8bab40e4d38d3da845053
Charles Morman <github@cmorman.com>
2024-11-20 19:29:33 -0700

    docs: add `Task` docstrings Ref #1

--- app/task.py
+++ app/task.py
@@ -1,5 +1,10 @@
 """
-Task Module
+Task Module:
+
+This module represents a task and defines the attributes it can have.
+
+Classes:
+    Task: Responsible for holding task-related information such as an ID, title, due date, etc.
 """
 
 from typing import Optional
@@ -7,6 +12,16 @@ from datetime import datetime
 
 
 class Task:
+    """Responsible for holding task-related information such as an ID, title, due date, etc.
+
+    Attributes:
+        next_task_id: Class attribute that tracks the next available `task_id` to ensure uniqueness across instances.
+        _task_id (int): The unique ID of the task, automatically generated using `Task.next_task_id`.
+        title (str): A short title or description of the task.
+        description (Optional[str], optional): A longer description of the task details. Defaults to None.
+        status (str): The current status of the task. Must be "Pending", "In Progress", or "Completed". Defaults to "Pending".
+        due_date (Optional[datetime], optional): The due date of the task. Defaults to current date/time.
+    """
 
     next_task_id: int = 0
 
@@ -15,13 +30,15 @@ class Task:
         title: str,
         description: Optional[str] = None,
         due_date: Optional[datetime] = None,
+        status: Optional[str] = None,
     ):
         """Constructs a basic Task.
 
         Args:
-            title (str): _description_
-            description (Optional[str], optional): _description_. Defaults to None.
-            due_date (Optional[datetime], optional): _description_. Defaults to None.
+            title (str): A short title or description of the task.
+            description (Optional[str], optional): A longer description of the task details. Defaults to None.
+            due_date (Optional[datetime], optional): The due date of the task. Defaults to None.
+            status (Optional[str], optional): The current status of the task. Can be "Pending", "In Progress", or "Completed".
         """
         self._task_id = Task.next_task_id
         self.title = title
