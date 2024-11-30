da6a43fa44b05ba7020de1e49153c54b78473f06
Charles Morman <github@cmorman.com>
2024-11-30 12:13:00 -0700

    feat: add `TaskManager` class with complete set of methods to CRUD tasks/task list.

--- app/task_manager.py
+++ app/task_manager.py
@@ -8,7 +8,9 @@ class TaskManager:
     def __init__(self, task_list: TaskList):
         self.task_list = task_list
 
-    def _update_task_attribute(self, identifier: Task | str | int, attribute: str, value):
+    def _update_task_attribute(
+        self, identifier: Task | str | int, attribute: str, value
+    ):
         try:
             setattr(self.get_task(identifier), attribute, value)
         except TaskNotFoundError:
@@ -24,7 +26,9 @@ class TaskManager:
     def set_task_description(self, identifier: Task | str | int, new_desc: str):
         self._update_task_attribute(identifier, "description", new_desc)
 
-    def set_task_due_date(self, identifier: Task | str | int, new_due_date: str | datetime):
+    def set_task_due_date(
+        self, identifier: Task | str | int, new_due_date: str | datetime
+    ):
         self._update_task_attribute(identifier, "due_date", new_due_date)
 
     def set_task_status(self, identifier: Task | str | int, new_status: str):
@@ -55,13 +59,27 @@ class TaskManager:
     def get_task_by_id(self, task_id: int) -> Optional[Task]:
         try:
             return self.task_list.get_task(task_id=task_id)
-        except TaskListError:
-            print("task not found by task id")
+        except TaskListError as exc:
+            print(f"{exc.value}")
             return None
 
     def get_task_by_index(self, task_index: int) -> Optional[Task]:
         try:
             return self.task_list.get_all_tasks[task_index]
-        except IndexError:
-            print("task not found by task index")
+        except IndexError as exc:
+            print(f"{exc.value}")
             return None
+
+    def add_task_to_list(self, task: Task):
+        try:
+            self.task_list.add_task(task)
+        except TaskListError as exc:
+            print(f"{exc.value}")
+
+
+    def delete_from_list(self, task: Task):
+        try:
+            self.task_list.delete_task(task)
+        except TaskListError as exc:
+            print(f"{exc.value}")
+
